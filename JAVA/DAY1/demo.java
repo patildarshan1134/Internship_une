70d506600e26e4af3506eefcac455a22d52b272f
Your Name <darshanspatil1134@gmail.com>
2025-06-09 04:12:56 -0700

    Day 6

---
+++ JAVA/DAY1/demo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Split {
+	
+		    public static void main(String[] args)
+		    {
+		        Scanner sc=new Scanner(System.in);
+		        int a=sc.nextInt();
+		        
+		    	System.out.println("Left Shift operation:"+(a<<2));
+		    	System.out.println("Right Shift operation:"+(a>>2));
+		    	
+		    	System.out.println("unsigned Right Shift operation:"+(a>>>2));
+		    }
+		
+	}
